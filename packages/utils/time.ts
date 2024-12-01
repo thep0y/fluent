83ae11113aca54bbb8ae3eb3bb2e237876a02841
Ryan Wang <thepoy@163.com>
2024-12-01 09:01:47 +0800

    feat(utils/time): add useInterval function and rename file from timeout.ts
    - Introduced a new utility function `useInterval` for setting up and canceling intervals.
    - Renamed the file from `timeout.ts` to `time.ts` to better reflect its expanded functionality.

--- packages/utils/time.ts
+++ packages/utils/time.ts
@@ -27,3 +27,29 @@ export const useTimeout = (): [
 
   return [setTimeoutFn, cancelTimeoutFn];
 };
+
+export const useInterval = (): [
+  (callback: () => void, delay: number) => void,
+  () => void,
+] => {
+  const [timeoutId, setTimeoutId] = createSignal<Timeout | null>(null);
+
+  const setIntervalFn = (callback: () => void, delay: number) => {
+    const id = setInterval(callback, delay);
+    setTimeoutId(id);
+  };
+
+  const cancelIntervalFn = () => {
+    const id = timeoutId();
+    if (id) {
+      clearTimeout(id);
+      setTimeoutId(null);
+    }
+  };
+
+  onCleanup(() => {
+    cancelIntervalFn();
+  });
+
+  return [setIntervalFn, cancelIntervalFn];
+};
