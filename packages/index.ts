9de0a0e67b9a967b98665462a9c97fa79c1567cc
Ryan Wang <thepoy@163.com>
2024-12-01 09:18:20 +0800

    feat(packages): export components and types
    - Export Button, Label, ProgressBar, and Spinner components
    - Include corresponding Props types for each component
    - Aggregate exports from utils

--- packages/index.ts
+++ packages/index.ts
@@ -1,1 +1,15 @@
 import "./index.scss";
+
+export * as Button from "./button";
+export type { ButtonProps } from "./button";
+
+export * as Label from "./label";
+export type { LabelProps } from "./label";
+
+export * as ProgressBar from "./progress";
+export type { ProgressBarProps } from "./progress";
+
+export * as Spinner from "./spinner";
+export type { SpinnerProps } from "./spinner";
+
+export * from "./utils";
