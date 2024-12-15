ed2eac875caec536dd8180dbb606d068eda3e50c
thepoy <thepoy@163.com>
2024-12-15 13:57:46 +0800

    fix(tooltip): correct tooltip positioning calculation
    - Introduced `Offset` type and `calculateOffset` function to handle tooltip positioning logic.
    - Replaced the switch-case logic in `calculatePosition` with a more dynamic approach using a record of positioning functions.
    - Added default positioning fallback in `calculateOffset`.
    - Returned a default invisible position if the tooltip is not rendered yet.

--- src/components/tooltip.tsx
+++ src/components/tooltip.tsx
@@ -1,24 +1,66 @@
+import {
+  TbArrowCurveLeft,
+  TbArrowCurveRight,
+  TbArrowNarrowUp,
+} from "solid-icons/tb";
+import { For } from "solid-js";
 import Tooltip from "~/components/tooltip";
 import { Button } from "~/index";
 
 const TooltipDemo = () => {
-  return (
-    <div>
-      <div style={{ margin: "40px" }}>
-        <Tooltip
-          content="add something"
-          relationship="label"
-          positioning="after"
-          visible
-        >
-          <Button>+</Button>
-        </Tooltip>
-      </div>
+  const positions = [
+    ["above-start", <TbArrowCurveLeft />],
+    ["above", <TbArrowNarrowUp />],
+    ["above-end", <TbArrowCurveRight />],
 
-      <div style={{ margin: "40px" }}>
-        <Tooltip content="add something" relationship="label" visible>
-          <Button>+</Button>
-        </Tooltip>
+    ["before-top", <TbArrowCurveRight style={{ rotate: "-90deg" }} />],
+    ["before", <TbArrowNarrowUp style={{ rotate: "-90deg" }} />],
+    ["before-bottom", <TbArrowCurveLeft style={{ rotate: "-90deg" }} />],
+
+    ["after-top", <TbArrowCurveLeft style={{ rotate: "90deg" }} />],
+    ["after", <TbArrowNarrowUp style={{ rotate: "90deg" }} />],
+    ["after-bottom", <TbArrowCurveRight style={{ rotate: "90deg" }} />],
+
+    ["below-start", <TbArrowCurveRight style={{ rotate: "180deg" }} />],
+    ["below", <TbArrowNarrowUp style={{ rotate: "180deg" }} />],
+    ["below-end", <TbArrowCurveLeft style={{ rotate: "180deg" }} />],
+  ] as const;
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        "align-items": "flex-start",
+        gap: "4px",
+        "flex-direction": "row",
+        margin: "16px  0",
+        height: "400px",
+      }}
+    >
+      <div
+        style={{
+          display: "grid",
+          margin: "24px 128px",
+          gap: "4px",
+          "grid-template-areas":
+            '".             above-start   above         above-end     .            "' +
+            '"before-top    .             .             .             after-top    "' +
+            '"before        .             .             .             after        "' +
+            '"before-bottom .             .             .             after-bottom "' +
+            '".             below-start   below         below-end     .            "',
+        }}
+      >
+        <For each={positions}>
+          {([position, icon]) => (
+            <Tooltip
+              positioning={position}
+              relationship="label"
+              content={`This is ${position} tooltip`}
+            >
+              <Button style={{ "grid-area": position }} icon={icon} />
+            </Tooltip>
+          )}
+        </For>
       </div>
     </div>
   );
