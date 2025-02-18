34d337157a7c262daceef874335d95afe3728712
thepoy <thepoy@163.com>
2025-02-18 15:07:35 +0800

    refactor(card): enhance component structure and props handling
    - Refactor `Card`, `CardFooter`, `CardHeader`, and `CardPreview` components to use `splitProps` for better props management.
    - Add `addClassList` utility for consistent class handling across components.
    - Improve type safety by extending `JSX.HTMLSlotElementAttributes<HTMLDivElement>`.
    - Ensure proper separation of local and other props for cleaner and more maintainable code.

--- packages/components/card/CardFooter/index.tsx
+++ packages/components/card/CardFooter/index.tsx
@@ -1,4 +1,6 @@
-import type { ParentProps } from "solid-js";
+import { splitProps, type JSX, type ParentProps } from "solid-js";
+
+import { addClassList } from "~/utils";
 
 import type { CardFooterProps } from "./types";
 
@@ -6,6 +8,23 @@ import "./index.scss";
 
 const baseClassName = "fluent-card-footer";
 
-export const CardFooter = (props: ParentProps<CardFooterProps>) => (
-  <div class={baseClassName}>{props.children}</div>
-);
+export const CardFooter = (
+  props: ParentProps<CardFooterProps> &
+    JSX.HTMLSlotElementAttributes<HTMLDivElement>,
+) => {
+  const [local, others] = splitProps(props, ["children", "class"]);
+
+  const classList = () =>
+    addClassList({
+      base: baseClassName,
+      others: {
+        [`${local.class}`]: local.class,
+      },
+    });
+
+  return (
+    <div {...others} classList={classList()}>
+      {local.children}
+    </div>
+  );
+};
